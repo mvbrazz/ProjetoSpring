7f8a044ffd4ba86c071df2a80d64982aa3f67488
mvbrazz <mvbrazz@hotmail.com>
2023-02-14 17:35:37 -0300

    Adicionando index: html , css e JS

---
+++ src/main/java/com/projeto/projeto/js/index.js
@@ -0,0 +1,15 @@
+function login(){
+
+    var inputUsuario = document.querySelector("#usuario");
+    var inputSenha = document.querySelector("#senha");
+
+    var usuario = inputUsuario.value;
+    var senha = inputSenha.value;
+    
+    console.log(usuario + senha);
+
+
+
+
+}
+  
